e233c37c0676fce72a395c0bcfa80a095287756e
Jasper Laurila <jasper.laurila@gmail.com>
2024-05-16 14:06:48 +0300

    updated eslint config to pupumoves pixijs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,13 @@
+esling.config.mjs
+
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+
+  {ignores: ["**/test/**", "**/tests/**", "**/*.test.js"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "module"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
 ];
