d8643416e568d3a5bec68333af3dc96002f322a0
TommiTH <tommithyyrylainen@gmail.com>
2024-05-16 12:43:56 +0300

    cleaned index.js slightly

--- src/index.js
+++ src/index.js
@@ -1,44 +1,48 @@
 import { initGrid, addToGrid, moveGridObjectToDir as tryMoveGridObjectToDir } from "./game/gamegrid.js";
 import { getNewGridObject } from "./game/gridobject.js";
-import {app, setBunnyPos} from "./game/rendering.js"
+import { app, setBunnyPos } from "./game/rendering.js"
 var pupu;
+
+main();
+
+function main() {
+    document.body.appendChild(app.canvas);
+    CreateMovePupuButtons();
+    InitGame();
+}
+
+function InitGame() {
+    initGrid(8, 8);
+    pupu = getNewGridObject("pupu");
+    addToGrid(pupu, 0, 0);
+}
+
 function onMovePupu(x, y) {
     console.log("MOVE");
     if (tryMoveGridObjectToDir(pupu, x, y)) {
-        //imo should be an event that rendering receives (or someone)
-        //reDrawPupu(pupu.cell.x, pupu.cell.y);
         setBunnyPos(x, y);
     }
+}
+
+function CreateMovePupuButtons() {
+    CreateButton("upButton", "UP");
+    CreateButton("downButton", "DOWN");
+    CreateButton("leftButton", "LEFT");
+    CreateButton("rightButton", "RIGHT");
+    createOnClickButtonDirectionEvent("upButton", 0, -1);
+    createOnClickButtonDirectionEvent("leftButton", -1, 0);
+    createOnClickButtonDirectionEvent("rightButton", 1, 0);
+    createOnClickButtonDirectionEvent("downButton", 0, 1);
+}
+
+function CreateButton(id, innerText) {
+    let button = document.createElement("button");
+    button.id = id;
+    button.innerText = innerText;
+    document.body.appendChild(button);
 }
 
 function createOnClickButtonDirectionEvent(buttonName, x, y) {
     let buttonInput = document.getElementById(buttonName);
     buttonInput.addEventListener("click", function () { onMovePupu(x, y) }, false);
 }
-
-document.body.appendChild(app.canvas);
-const upButton = document.createElement("button");
-const leftButton = document.createElement("button");
-const rightButton = document.createElement("button");
-const downButton = document.createElement("button");
-upButton.innerText = "UP";
-upButton.id = "upButton";
-downButton.innerText = "DOWN"
-downButton.id = "downButton";
-leftButton.innerText = "LEFT"
-leftButton.id = "leftButton";
-rightButton.innerText = "RIGHT"
-rightButton.id = "rightButton";
-document.body.appendChild(upButton);
-document.body.appendChild(leftButton);
-document.body.appendChild(rightButton);
-document.body.appendChild(downButton);
-
-initGrid(8, 8);
-pupu = getNewGridObject("pupu");
-addToGrid(pupu, 0, 0);
-
-createOnClickButtonDirectionEvent("upButton", 0, -1);
-createOnClickButtonDirectionEvent("leftButton", -1, 0);
-createOnClickButtonDirectionEvent("rightButton", 1, 0);
-createOnClickButtonDirectionEvent("downButton", 0, 1);
