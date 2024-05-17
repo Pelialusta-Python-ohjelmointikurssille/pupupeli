29a7b6861f4f3cb988014b214f32623d0669bec9
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-05-17 14:01:18 +0300

    Added bunny move animations

--- src/game/rendering.js
+++ src/game/rendering.js
@@ -1,6 +1,8 @@
 import * as PIXI from "pixi.js"
 import { GridSpaceToScreenSpace } from "./coord_helper.js"
 
+//refactor pls
+
 export const app = new PIXI.Application();
 await app.init({
   width: 640,
@@ -26,18 +28,48 @@ bunny_sprite.anchor.set(0.5);
 app.stage.addChild(background_sprite);
 app.stage.addChild(bunny_sprite);
 
-var bx = 0;
-var by = 0;
+var gridX = 0;
+var gridY = 0;
+let coords = GridSpaceToScreenSpace(gridX, gridY, 640, 640, 8, 8, 0, 0);
+var targetX = coords[0];
+var targetY = coords[1];
+var xdir = 0;
+var ydir = 0;
+var bunnySpeedMod = 0.1;
 
-export function setBunnyPos(x, y) {
-  bx += x;
-  by += y;
+bunny_sprite.x = targetX;
+bunny_sprite.y = targetY;
+
+var atTarget = true;
+
+function GetDistance(ax, ay, bx, by) {
+  return Math.sqrt((bx-ax)**2 + (by-ay)**2);
 }
 
+export function setBunnyPos(x, y) {
+  gridX += x;
+  gridY += y;
+  let coords = GridSpaceToScreenSpace(gridX, gridY, 640, 640, 8, 8, 0, 0);
+  targetX = coords[0];
+  targetY = coords[1];
+  xdir = ((coords[0]-bunny_sprite.x));
+  ydir = ((coords[1]-bunny_sprite.y));
+  atTarget = false;
+}
 
-app.ticker.add(() =>
-{
-  let coords = GridSpaceToScreenSpace(bx, by, 640, 640, 8, 8, 0, 0);
-  bunny_sprite.position.x = coords[0];
-  bunny_sprite.position.y = coords[1];
+app.ticker.add((time) =>
+{  
+  if (atTarget == false){
+    if (
+        GetDistance(bunny_sprite.x, bunny_sprite.y, targetX, targetY) <= 
+        GetDistance(bunny_sprite.x+(xdir*time.deltaTime * bunnySpeedMod), bunny_sprite.y+(ydir*time.deltaTime * bunnySpeedMod), targetX, targetY)
+      ){
+      bunny_sprite.position.x = targetX;
+      bunny_sprite.position.y = targetY;
+      atTarget = true;
+    } else {
+      bunny_sprite.position.x += xdir * time.deltaTime * bunnySpeedMod;
+      bunny_sprite.position.y += ydir * time.deltaTime * bunnySpeedMod;
+    }
+  }
 });
