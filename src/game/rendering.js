ca28845796c342ad058b4c071c73146f12447e16
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-05-20 12:50:25 +0300

    PixiJS no longer requires Webpack

--- src/game/rendering.js
+++ src/game/rendering.js
@@ -1,4 +1,3 @@
-import * as PIXI from "pixi.js"
 import { GridSpaceToScreenSpace } from "./coord_helper.js"
 
 //refactor pls
@@ -13,8 +12,8 @@ await app.init({
 })
 app.ticker.maxFPS = 60;
 
-const bunny_texture = await PIXI.Assets.load("static/game_assets/pupu_edesta_lapinakyva.png");
-const background_texture = await PIXI.Assets.load("static/game_assets/Tausta1.png");
+const bunny_texture = await PIXI.Assets.load("src/static/game_assets/pupu_edesta_lapinakyva.png");
+const background_texture = await PIXI.Assets.load("src/static/game_assets/Tausta1.png");
 
 const bunny_sprite = new PIXI.Sprite(bunny_texture);
 const background_sprite = new PIXI.Sprite(background_texture);
