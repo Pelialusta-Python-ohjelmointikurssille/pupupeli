bcd269364c26bb3e6ef6fb2208d17d8aa1d7ec8e
TommiTH <TommiTHyyrylainen@gmail.com>
2024-05-21 10:44:16 +0300

    Cleaned up editor.js

--- src/editor.js
+++ src/editor.js
@@ -2,44 +2,48 @@
 /**
  * This file contains the code for the editor and Python code execution
  */
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/dawn");
-editor.session.setMode("ace/mode/python");
-editor.setOptions({
-    autoScrollEditorIntoView: true,
-    copyWithEmptySelection: true,
-});
-
-main();
-
 var pyodide;
+var editor;
 
-// write doc for start
-/**
- * Starts the Python code execution
- */
-function start() {
-    console.log(editor.getValue());
-    test(editor.getValue());
-}
-
-// write doc for test
-/**
- * Tests the Python code
- * @param {string} string - The Python code to test
- */
-function test(string) {
-    pyodide.runPython(`
-        import sys
-        sys.version
-    `);
-    pyodide.runPython(string);
-}
+initializePyodide();
+initializeEditor();
 
 // write doc for main
 /**
  * Initializes the Pyodide module
  */
-async function main() {
+async function initializePyodide() {
     pyodide = await loadPyodide();
+}
+
+function initializeEditor() {
+    console.log("initialized Editor");
+    editor = ace.edit("editor");
+    editor.setTheme("ace/theme/dawn");
+    editor.session.setMode("ace/mode/python");
+    editor.setOptions({
+        autoScrollEditorIntoView: true,
+        copyWithEmptySelection: true,
+    });
+}
+
+// write doc for start
+/**
+ * Starts the Python code execution
+ */
+function onClickRunCodeButton() {
+    runPythonCode(editor.getValue());
+}
+
+// write doc for test
+/**
+ * Tests the Python code
+ * @param {string} string - The Python code to run
+ */
+function runPythonCode(string) {
+    pyodide.runPython(`
+        import sys
+        sys.version
+    `);
+    pyodide.runPython(string);
 }
