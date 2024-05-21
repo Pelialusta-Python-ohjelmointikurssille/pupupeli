26452119e8e6ab892274121318001147bc2e7aaf
Elias Perttilä <epertt@gmail.com>
2024-05-21 11:02:14 +0000

    remove outdated documentation

--- src/editor.js
+++ src/editor.js
@@ -1,19 +1,11 @@
 import { moveBunny } from "./index.js"
 
-// write doc for this file
-/**
- * This file contains the code for the editor and Python code execution
- */
 var pyodide;
 var editor;
 
 initializePyodide();
 initializeEditor();
 
-// write doc for main
-/**
- * Initializes the Pyodide module
- */
 async function initializePyodide() {
     pyodide = await loadPyodide();
     pyodide.setStdin();
@@ -30,19 +22,10 @@ function initializeEditor() {
     });
 }
 
-// write doc for start
-/**
- * Starts the Python code execution
- */
-function onClickRunCodeButton() {
+export function onClickRunCodeButton() {
     runPythonCode(editor.getValue());
 }
 
-// write doc for test
-/**
- * Tests the Python code
- * @param {string} string - The Python code to run
- */
 function runPythonCode(string) {
     registerJSModules()
     pyodide.runPython(`
