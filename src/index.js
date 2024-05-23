ca80d7919102fcfe9fd5c2aebe055fbc2c5b7fbf
Elias Perttilä <epertt@gmail.com>
2024-05-23 09:40:24 +0000

    remove unnecessary console.log

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ async function main() {
 async function CreateGameWindow() {
     let app = await InitGame();
     let canvas = app.canvas;
-
+    
     document.getElementById("left-container").insertAdjacentElement("afterend", canvas);
     canvas.classList.add("is-flex");
     canvas.id = "game";
