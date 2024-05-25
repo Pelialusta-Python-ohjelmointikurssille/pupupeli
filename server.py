302b47eaea230d6232329785dc66c0cee1719cb0
Elias Perttilä <epertt@gmail.com>
2024-05-25 05:44:02 +0300

    add server.py for testing

---
+++ server.py
@@ -0,0 +1,13 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
+        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+        super().end_headers()
+
+if __name__ == '__main__':
+    server_address = ('', 8989)
+    httpd = HTTPServer(server_address, CORSRequestHandler)
+    print("Serving on port 8989...")
+    httpd.serve_forever()
