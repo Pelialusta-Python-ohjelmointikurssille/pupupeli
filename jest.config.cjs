169adf83415fa3452d4dcd18faa1d428c3276b4b
epertt <epertt@gmail.com>
2024-05-27 22:20:40 +0300

    ignore worker.js instead of pyodide.js in jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -22,7 +22,7 @@ module.exports = {
     collectCoverageFrom: [
         "src/**/*.js",
         "!src/**/rendering.js", // Exclude specific file
-        "!src/input/pyodide.js", // Exclude specific file
+        "!src/input/worker.js",
         "!src/**/index.js",
         "!src/**/game.js",
         "!src/**/editor.js",
