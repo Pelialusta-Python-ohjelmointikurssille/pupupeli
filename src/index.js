b473ea4a23c229cff0e36e1d212302809d35fd3a
epertt <epertt@gmail.com>
2024-05-30 02:12:24 +0300

    fix bug that happens when you spam next step after resetting

--- src/index.js
+++ src/index.js
@@ -140,7 +140,6 @@ function nextStepButtonClick() {
     onRunButtonClick();
     eventHandler.runSingleCommand();
     if (currentState === runningState) onRunButtonClick();
-
 }
 
 export function displayErrorMessage(error) {
