8c1fa2ab8c9a9a169abeb6b49c600166868889b0
Joonas Honkavaara <joonas.honkavaara@hotmail.com>
2024-05-30 15:19:56 +0300

    Added player parameter to Grid class

--- src/newGame/gridfactory.js
+++ src/newGame/gridfactory.js
@@ -2,8 +2,9 @@ import { getNewGridObject } from "./gridobject.js";
 import { Grid } from "./grid.js";
 
 export function getNewGameGrid() {
-    let newGrid = new Grid(8, 8);
     let pelaaja = getNewGridObject("pupu");
+    let newGrid = new Grid(pelaaja, 8, 8);
+    
     newGrid.addToGrid(pelaaja, 0, 0);
     consoleDebugDoubleArray(newGrid.doubleArray);
     console.log(newGrid.getWidth() + ", " + newGrid.getHeight());
