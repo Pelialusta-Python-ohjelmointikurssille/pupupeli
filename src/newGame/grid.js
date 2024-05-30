1992da61b7f2a430f031d5a91e934c92b6a6b10b
Joonas Honkavaara <joonas.honkavaara@hotmail.com>
2024-05-30 15:01:13 +0300

    Added functions from gamegrid file to grid class and made minor fixes to new_game.js

--- src/newGame/grid.js
+++ src/newGame/grid.js
@@ -1,20 +1,53 @@
 export class Grid {
-    constructor(doubleArray) {
-        this.doubleArray = doubleArray;
-        this.objects = []
+    constructor(width, height) {
+        this.doubleArray = this.CreateDoubleArray(width, height);
+        this.objects = [];
+        this.width = width;
+        this.height = height;
+
     };
-
-    getWidth() {
-        return this.doubleArray.length;
-    }
-
-    getHeight() {
-        return this.doubleArray[0].length;
-    }
 
     addToGrid(gridObject, x, y) {
         this.doubleArray[x][y].entities.push(gridObject);
         gridObject.cell = this.doubleArray[x][y];
+    }
+    moveGridObjectToDir(gridObject, direction) {
+        if (gridObject == null) return false;
+        let dirVector = Vector2.FromDirection(direction);
+        let newX = gridObject.cell.x + dirVector.x;
+        let newY = gridObject.cell.y + dirVector.y;
+        if (boundaryCheck(newX, newY) == false) return false; 
+        removeFromGrid(gridObject);
+        addToGrid(gridObject, newX, newY);
+        return true;
+    }
+    boundaryCheck(x, y) {
+        if (x < 0 | x >= grid.length) return false;
+        if (y < 0 | y >= grid[0].length) return false;
+        return true;
+    }
+    addToGrid(gridObject, x, y) {
+        grid[x][y].entities.push(gridObject);
+        gridObject.cell = grid[x][y];
+    }
+    removeFromGrid(gridObject) {
+        let x = gridObject.cell.x;
+        let y = gridObject.cell.y;
+        let cell = grid[x][y];
+        let index = cell.entities.indexOf(gridObject);
+        cell.entities.splice(index, 1);
+        gridObject.cell = null;
+    }
+    CreateDoubleArray(width, height) {
+        //js doesn't have double arrays T:Tommi
+        let newGrid = [];
+        for (let x = 0; x < width; x++) {
+            newGrid[x] = [];
+            for (let y = 0; y < height; y++) {
+                newGrid[x][y] = new Cell(x, y);
+            }
+        }
+        return newGrid;
     }
 
 };
--- src/newGame/gridfactory.js
+++ src/newGame/gridfactory.js
@@ -1,9 +1,8 @@
 import { getNewGridObject } from "./gridobject.js";
 import { Grid } from "./grid.js";
-import { Cell } from "./cell.js";
 
 export function getNewGameGrid() {
-    let newGrid = new Grid(CreateDoubleArray(8, 8));
+    let newGrid = new Grid(8, 8);
     let pelaaja = getNewGridObject("pupu");
     newGrid.addToGrid(pelaaja, 0, 0);
     consoleDebugDoubleArray(newGrid.doubleArray);
@@ -11,24 +10,6 @@ export function getNewGameGrid() {
     return newGrid;
 };
 
-// write doc for CreateDoubleArray
-/** 
- * Creates a double array with the specified dimensions
- * @param {number} width - The width of the array
- * @param {number} height - The height of the array
- * @returns {Array} - The double array
- */
-function CreateDoubleArray(widht, height) {
-    //js doesn't have double arrays T:Tommi
-    let newGrid = [];
-    for (let x = 0; x < widht; x++) {
-        newGrid[x] = [];
-        for (let y = 0; y < height; y++) {
-            newGrid[x][y] = new Cell(x, y);
-        }
-    }
-    return newGrid;
-}
 
 function consoleDebugDoubleArray(doubleArray) {
     for (let x = 0; x < doubleArray.length; x++) {
