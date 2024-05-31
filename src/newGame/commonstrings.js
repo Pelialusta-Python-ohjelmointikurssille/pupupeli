fdf20671c2ec6b54f8b0d26bad913ae9199a1983
TommiTH <TommiTHyyrylainen@gmail.com>
2024-05-31 13:05:32 +0300

    Added the other strings and new string, "move"

--- src/newGame/commonstrings.js
+++ src/newGame/commonstrings.js
@@ -1,18 +1,22 @@
 const PLAYER_STR = "player";
 const TYPE_STR = "type";
 const GRID_STR = "grid";
+const MOVE_STR = "move";
 
 export class Constants {
 
     static get PLAYER_STR() {
         return PLAYER_STR;
     } 
-    static get TYPE_STR_STR() {
-        return PLAYER_STR;
+    static get TYPE_STR() {
+        return TYPE_STR;
     } 
-    static get GRID_STR_STR() {
-        return PLAYER_STR;
-    } 
+    static get GRID_STR() {
+        return GRID_STR;
+    }
+    static get MOVE_STR() {
+        return MOVE_STR;
+    }
 }
 
 
