e71f4dc8b378112e8252198997a5985b7001795f
TommiTH <TommiTHyyrylainen@gmail.com>
2024-05-31 12:43:24 +0300

    made so that gridfactory uses commonstrings.js Constants class instead of having it's own string const

--- src/newGame/gridfactory.js
+++ src/newGame/gridfactory.js
@@ -2,15 +2,10 @@ import { getNewGridObject } from "./gridobject.js";
 import { Grid } from "./grid.js";
 import { Constants } from "./commonstrings.js";
 
-//Types for gridobjects
-//TODO: make these local so everyone no need to manually type types.
-const playerType = "player";
-
 export function getNewGameGrid() {
-    let player = getNewGridObject(playerType);
+    let player = getNewGridObject(Constants.PLAYER_STR);
     let newGrid = new Grid(player, 8, 8);
     newGrid.addToGrid(player, 0, 0);
     newGrid.addToGrid(player, 0, 0);
     return newGrid;
 };
-console.log(Constants.PLAYER_STR);
