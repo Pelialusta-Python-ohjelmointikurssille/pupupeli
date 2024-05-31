01fcbdcb5d91eb04b0a2cf0e88c2b24f7587bfce
Joonas Honkavaara <joonas.honkavaara@hotmail.com>
2024-05-31 12:41:21 +0300

    Added class containing common constant strings to avoid naming errors

---
+++ src/newGame/commonstrings.js
@@ -0,0 +1,22 @@
+const PLAYER_STR = "player";
+const TYPE_STR = "type";
+const GRID_STR = "grid";
+
+export class Constants {
+
+    static get PLAYER_STR() {
+        return PLAYER_STR;
+    } 
+    static get TYPE_STR_STR() {
+        return PLAYER_STR;
+    } 
+    static get GRID_STR_STR() {
+        return PLAYER_STR;
+    } 
+}
+
+
+
+
+    
+   
