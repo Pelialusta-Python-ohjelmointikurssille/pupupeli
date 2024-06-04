9cfa2495877799d8db751c71ff66adec68f85f87
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-06-04 11:14:24 +0300

    Fixed background scaling

--- src/newGame/graphics_handler/graphics_handler.js
+++ src/newGame/graphics_handler/graphics_handler.js
@@ -29,7 +29,7 @@ export class GraphicsHandler {
 
         this.createGrid();
 
-        this.createEntity("bgtest", "background", { bgWidth: this.gridHeight * 128, bgHeight: this.gridWidth * 128});
+        this.createEntity("bgtest", "background", { bgWidth: this.gridWidth * 128, bgHeight: this.gridHeight * 128});
         let t2 = new Date().getTime();
         console.log(`Loading graphics engine took ${t2-t1}ms`);
     }
