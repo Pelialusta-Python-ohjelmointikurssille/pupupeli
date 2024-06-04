85a8bb5731b11faea9d840a63a6cab6e22216cdc
TommiTH <TommiTHyyrylainen@gmail.com>
2024-06-04 14:48:28 +0300

    added/modified docstrings, added Command interface, fixed a bug resulting from not giving the id in this.graphicsHandler.doAction()

--- src/game/commands.js
+++ src/game/commands.js
@@ -1,9 +1,17 @@
 import { Constants, GetDirectionAsString } from "./commonstrings.js";
-//I dont know how to make a Command interface in javascript that has nice syntax, so I just pretend that all classes in this file
-//implements the interface Command. That just means that all these classes have the function "execute()" (and can easily implemet "undo()").
+//implements the interface Command. That just means that all these classes have the function "execute()" (and can easily implement "undo()").
 //Stuff to read if interested: 
 //    https://refactoring.guru/design-patterns/command
 //    https://gameprogrammingpatterns.com/command.html
+
+
+/**
+ * The interface for commands
+ */
+const Command = {
+    execute: function () { },
+    //undo: function () { }, maybe someday
+};
 
 /**
  * MoveCommand is created to execute the MoveCommand action in game logic and rendering.
@@ -47,8 +55,8 @@ export class MoveCommand {
         let gridobjects = this.grid.getAdjacentObjectsAtDir(this.moveStartPos.x, this.moveStartPos.y, this.dir);
         for (let i = 0; i < gridobjects.length; i++) {
             if (gridobjects[i].type === Constants.COLLECTIBLE) {
-                go = gridobjects[i];
-                this.graphicsHandler.doAction(go, "hide", { time: this.objectHideSpeed });
+                let go = gridobjects[i];
+                this.graphicsHandler.doAction(go.id, "hide", { time: this.objectHideSpeed });
                 this.grid.removeFromGrid(go);
             }
         }
