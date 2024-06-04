85f991f63bb5546282fe07972f3e7b2db874f4ee
thefakejj <jasper.laurila@gmail.com>
2024-06-04 10:56:59 +0300

    ignored some rendering for tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -28,10 +28,18 @@ module.exports = {
         "!src/input/worker.js",
         "!src/event_handler.js",
         "!src/**/index.js",
+        "!src/**/ui.js",
         "!src/**/game.js",
         "!src/**/editor.js",
+        "!src/**/graphics_camera_entity.js",
+        "!src/**/graphics_entity_factory.js",
+        "!src/**/graphics_entity_handler.js",
+        "!src/**/graphics_handler.js",
+        "!src/**/move_tween.js",
+        "!src/**/pixi_renderer.js",
         "!src/**/example/**", // Exclude all files in specific directory
         "!src/**/objects/**",
+        "!src/**/graphics_handler/entities/**",
         "!src/**/tests/**"
     ]
 };
