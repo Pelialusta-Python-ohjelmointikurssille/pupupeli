f52624ad5ccf4864ff2d941750b5e4fb218ec92d
Ville-Petteri Juselius <vp.juselius@gmail.com>
2024-06-06 10:31:34 +0300

    Dynamic button count

--- src/file_reader.js
+++ src/file_reader.js
@@ -49,3 +49,51 @@ export function tryGetFileAsJson(path) {
     const result = JSON.parse(response);
     return result;
 }
+
+    export function countForFilesInDirectory(directory) {
+        let fileNumber = 1;
+
+        // Check files from 1 to n in a for loop
+        while (checkIfFileExists(`${directory}/${fileNumber}.json`) !== null) {
+            fileNumber++;
+        }
+
+        // The number of files is one less than the first file that was not found
+        return fileNumber - 1;
+    }
+
+    export function countFilesInDirectory(directory) {
+        let low = 1;
+        let high = 2;
+        let middle;
+
+        // Expand the search range until a file is not found
+        while (checkIfFileExists(`${directory}/${high}.json`) !== null) {          
+            low = high;
+            high *= 2;
+            console.log("moi" + high)
+        }
+
+        // Perform a binary search to find the last file
+        while (low < high) {
+            middle = Math.floor((low + high) / 2);
+
+            if (checkIfFileExists(`${directory}/${middle}.json`) !== null) {
+                low = middle + 1;
+            } else {
+                high = middle;
+            }
+        }
+
+        // The number of files is one less than the first file that was not found
+        return low - 1;
+    }
+
+    function checkIfFileExists(path) {
+        try {
+            return tryGetFileAsJson(path);
+        } catch (error) {
+            console.log("File not found: " + path)
+            return null;
+        }
+    }
