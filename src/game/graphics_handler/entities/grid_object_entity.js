1900f03e823093e21ca541c2b1f7223d2bf2059c
TommiTH <tommithyyrylainen@gmail.com>
2024-06-06 14:24:32 +0300

    animations on GOEntities are now null when finished, seemed bugless

--- src/game/graphics_handler/entities/grid_object_entity.js
+++ src/game/graphics_handler/entities/grid_object_entity.js
@@ -9,7 +9,7 @@ export class GridObjectEntity extends GraphicsEntity {
         this.gridCellPosition = new Vector2(0, 0);
         this.sizeWithinCellMultiplier = 0.9;
         this.fakeZPosition = 0;
-        this.currentAnimation = null;;
+        this.currentAnimation = null;
         this.type = "grid_object";
         if (data !== null) {
             if (data.position !== null) {
@@ -45,6 +45,7 @@ export class GridObjectEntity extends GraphicsEntity {
 
     onFinishAnimation(name) {
         this.isReady = true;
+        this.currentAnimation = null;
         console.log("FINISH ANIM " + name)
     }
 
