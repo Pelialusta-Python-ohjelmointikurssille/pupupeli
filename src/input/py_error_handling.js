f51fac2f83a2d329056f45de20e320a8c3593a44
Joonas Honkavaara <joonas.honkavaara@hotmail.com>
2024-06-06 11:47:51 +0300

    Created function and translations-dictionary  to translate error messages to Finnish

--- src/input/py_error_handling.js
+++ src/input/py_error_handling.js
@@ -18,10 +18,23 @@ export function extractErrorDetails(errorMessage) {
     } else {
         lineNumberMatch = lastLineReference;
     }
+    
+    const translatedErrorType = translateErrorType(errorTypeMatch)
 
-    if (errorTypeMatch && lineNumberMatch) {
-        return { text: errorTypeMatch, line: lineNumberMatch };
+    if (translatedErrorType && lineNumberMatch) {
+        return { text: translatedErrorType, line: lineNumberMatch };
     } else {
-        return { text: errorMessage, line: "Unknown Line" };
+        return { text: errorMessage, line: "Tuntematon rivi" };
     }
+}
+
+function translateErrorType(errorType) {
+    const translations = {
+        "SyntaxError: invalid syntax": "Kirjoititko koodisi varmasti oikein?",
+        "ValueError: Virheellinen suunta": "Antamasi suunta ei ole kirjoitettu oikein",
+        "IndexError: list index out of range": "Yritit käyttää listan kohtaa, jota ei ole olemassa. Tarkista listan pituus ja yritä uudelleen!"
+
+    };
+
+    return translations[errorType] || errorType;
 }
