6707257e16cd2c7306ba0322dd2cd79c2d150547
Joonas Honkavaara <joonas.honkavaara@hotmail.com>
2024-06-06 13:48:08 +0300

    Translations added for more error types

--- src/input/py_error_handling.js
+++ src/input/py_error_handling.js
@@ -2,22 +2,13 @@ export function extractErrorDetails(errorMessage) {
     const regex = /File .*?, line (\d+)/g;
     let match;
     let lastLineReference;
-    // let lastToLastLineReference;
 
     const lines = errorMessage.split('\n');
     const errorTypeMatch = lines[lines.length - 2];
 
     while ((match = regex.exec(errorMessage)) !== null) {
-        // lastToLastLineReference = lastLineReference;
         lastLineReference = match[1];
     }
-
-    // if (errorTypeMatch === "ValueError: Virheellinen suunta") {
-    //     // lineNumberMatch = lastToLastLineReference;
-    //     lineNumberMatch = lastLineReference
-    // } else {
-    //     lineNumberMatch = lastLineReference;
-    // }
 
     const lineNumberMatch = lastLineReference;
     const translatedErrorType = translateErrorType(errorTypeMatch)
@@ -31,22 +22,26 @@ export function extractErrorDetails(errorMessage) {
 
 function translateErrorType(errorType) {
     const translations = {
-        "SyntaxError: invalid syntax": "Kirjoititko koodisi varmasti oikein?",
+        "SyntaxError: invalid syntax": "Koodistasi löytyy kirjoitusvirhe",
         "ValueError: Virheellinen suunta": "Antamasi suunta ei ole kirjoitettu oikein",
-        "IndexError: list index out of range": "Yritit käyttää listan kohtaa, jota ei ole olemassa. Tarkista listan pituus ja yritä uudelleen!"
+        "IndexError: list index out of range": "Yritit käyttää listan kohtaa, jota ei ole olemassa. Tarkista listan pituus ja yritä uudelleen"
 
     };
 
     if (errorType.startsWith("NameError: name")) {
-        return "Käytit nimeä, jota ei ole määritelty. Tarkista kirjoitusvirheet!";
+        return "Käytit nimeä, jota ei ole määritelty. Tarkista kirjoitusvirheet";
     }
     
     if (errorType.startsWith("ModuleNotFoundError: No module named")) {
-        return "Yritit käyttää moduulia, jota ei löydy. Tarkista moduulin nimi!";
+        return "Yritit käyttää moduulia, jota ei löydy. Tarkista moduulin nimi";
     }
 
     if (errorType.startsWith("IndentationError: expected an indented block")) {
-        return "Koodisi sisennyksessä on ongelmia. Tarkista, että jätät rivin alkuun tyhjää tilaa!";
+        return "Tarkista, että jätät rivin alkuun tyhjää tilaa";
+    }
+
+    if (errorType.startsWith("TypeError:")) {
+        return "Tarkista, että käyttämäsi arvot ovat oikeaa tyyppiä";
     }
 
     return translations[errorType] || errorType;
