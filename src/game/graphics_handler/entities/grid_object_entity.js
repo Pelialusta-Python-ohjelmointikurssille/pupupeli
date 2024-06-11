724859cfdb2cb30a276941bc85ab78147a8bc809
thefakejj <jasper.laurila@gmail.com>
2024-06-11 10:51:45 +0300

    funny lines

--- src/game/graphics_handler/entities/grid_object_entity.js
+++ src/game/graphics_handler/entities/grid_object_entity.js
@@ -1,6 +1,7 @@
 import { Vector2 } from "../../vector.js";
 import { GraphicsEntity } from "./graphics_entity.js";
 import { Constants } from "../../commonstrings.js";
+import * as PIXI from "https://cdnjs.cloudflare.com/ajax/libs/pixi.js/8.1.5/pixi.mjs";
 
 export class GridObjectEntity extends GraphicsEntity {
     constructor(entityId, entityHandler, container, sprite, data) {
@@ -10,12 +11,25 @@ export class GridObjectEntity extends GraphicsEntity {
         this.sizeWithinCellMultiplier = 0.9;
         this.fakeZPosition = 0;
         this.currentAnimation = null;
-        this.usesTrail = false;
+        this.usesTrail = true;
         this.type = "grid_object";
         if (data != null) {
             if (data.position != null) {
                 this.gridCellPosition = data.position;
             }
+        }
+        if (this.usesTrail === true) {
+            this.realPath = new PIXI.Graphics();
+            this.realPath.moveTo(0, 0);
+            this.realPath.lineTo(100, 200);
+            this.realPath.lineTo(200, 200);
+            this.realPath.lineTo(240, 100);
+            this.realPath.stroke({ width: 2, color: 0xffffff });
+
+            this.realPath.position.x = 50;
+            this.realPath.position.y = 50;
+
+            this.container.addChild(this.realPath);
         }
         this.startPosition = new Vector2(this.gridCellPosition.x, this.gridCellPosition.y);
         this.screenPosition = this.gridReference.gridToScreenCoordinates(this.gridCellPosition);
