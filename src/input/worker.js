30d4e126a8a278445bc6f1d50bab43c2cfbfa30c
TommiTH <TommiTHyyrylainen@gmail.com>
2024-06-12 11:14:56 +0300

    added a undefined check to addLineNumberOutputs

--- src/input/worker.js
+++ src/input/worker.js
@@ -175,6 +175,8 @@ async function checkClearedConditions(codeString) {
 }
 
 function addLineNumberOutputs(codeString) {
+    if (codeString === undefined) return;
+    console.log(codeString);
     let lines = codeString.split('\n');
     let lastIndentation = '';
     lines = lines.map((line, index) => {
