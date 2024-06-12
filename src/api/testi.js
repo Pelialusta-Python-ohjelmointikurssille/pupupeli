9e7d2fbc8b6dd5838d49ddc805da91928cab7e2e
Joonas Honkavaara <joonas.honkavaara@hotmail.com>
2024-06-12 11:24:04 +0300

    Added test login function to get token

--- src/api/testi.js
+++ src/api/testi.js
@@ -10,3 +10,25 @@ async function getCompletedTasks(token) {
         throw new Error('Failed to retrieve completed tasks');
     }
 }
+async function login(username, password) {
+    const response = await fetch('https://tie.koodariksi.fi/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        const token = data.token;
+
+        localStorage.setItem('authToken', token);
+
+        return token;
+    } else {
+        throw new Error('Login failed');
+    }
+}
+
+
