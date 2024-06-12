dbe369f66e58345735af16598eae8930e5493c73
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-06-12 13:40:26 +0300

    Refactored background entity

--- src/game/graphics_handler/entities/background_entity.js
+++ src/game/graphics_handler/entities/background_entity.js
@@ -3,15 +3,12 @@ import { GraphicsEntity } from "./graphics_entity.js";
 export class BackgroundEntity extends GraphicsEntity {
     constructor(entityId, entityHandler, container, sprite, data, skins) {
         super(entityId, entityHandler, container, sprite, data, skins);
-        this.entityId = entityId;
-        this.entityHandler = entityHandler;
-        this.container = container;
-        this.sprite = sprite;
-        this.isReady = true;
         if (this.sprite !== null) {
-            this.container.addChild(this.sprite);
             this.sprite.width = data.bgWidth;
             this.sprite.height = data.bgHeight;
+            this.sprite.anchor.set(0.5)
+            this.sprite.x += this.sprite.width / 2;
+            this.sprite.y += this.sprite.height / 2;
         }
     }
 }
