02ab634c83dd16d89b5732945c666e13324e4e94
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-06-12 13:43:51 +0300

    Removed anchor change in background

--- src/game/graphics_handler/entities/background_entity.js
+++ src/game/graphics_handler/entities/background_entity.js
@@ -6,9 +6,6 @@ export class BackgroundEntity extends GraphicsEntity {
         if (this.sprite !== null) {
             this.sprite.width = data.bgWidth;
             this.sprite.height = data.bgHeight;
-            this.sprite.anchor.set(0.5)
-            this.sprite.x += this.sprite.width / 2;
-            this.sprite.y += this.sprite.height / 2;
         }
     }
 }
