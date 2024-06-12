d29d323c0c5dcf769084dd1122ef5217eec7e9fe
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-06-12 11:09:32 +0300

    Assets now follow a standard naming/organizing convention

--- src/game/graphics_handler/manifests/asset_manifest.js
+++ src/game/graphics_handler/manifests/asset_manifest.js
@@ -5,35 +5,35 @@ export const ASSETS = {
             assets: [
                 {
                     alias: "bunny_down",
-                    src: "src/static/game_assets/bunny_front.png"
+                    src: "src/static/game_assets/characters/bunny_down.png"
                 },
                 {
                     alias: "bunny_right",
-                    src: "src/static/game_assets/bunny_right.png"
+                    src: "src/static/game_assets/characters/bunny_right.png"
                 },
                 {
                     alias: "bunny_left",
-                    src: "src/static/game_assets/bunny_left.png"
+                    src: "src/static/game_assets/characters/bunny_left.png"
                 },
                 {
                     alias: "bunny_up",
-                    src: "src/static/game_assets/bunny_back.png"
+                    src: "src/static/game_assets/characters/bunny_up.png"
                 },
                 {
                     alias: "robot_down",
-                    src: "src/static/game_assets/robot_front.png"
+                    src: "src/static/game_assets/characters/robot_down.png"
                 },
                 {
                     alias: "robot_right",
-                    src: "src/static/game_assets/robot_right.png"
+                    src: "src/static/game_assets/characters/robot_right.png"
                 },
                 {
                     alias: "robot_left",
-                    src: "src/static/game_assets/robot_left.png"
+                    src: "src/static/game_assets/characters/robot_left.png"
                 },
                 {
                     alias: "robot_up",
-                    src: "src/static/game_assets/robot_back.png"
+                    src: "src/static/game_assets/characters/robot_up.png"
                 }
             ]
         },
@@ -42,11 +42,11 @@ export const ASSETS = {
             assets: [
                 {
                     alias: "grass",
-                    src: "src/static/game_assets/background_grass.png"
+                    src: "src/static/game_assets/backgrounds/background_grass.png"
                 },
                 {
                     alias: "metal",
-                    src: "src/static/game_assets/background_metal.png"
+                    src: "src/static/game_assets/backgrounds/background_metal.png"
                 }
             ]
         },
@@ -61,7 +61,7 @@ export const ASSETS = {
                     Mainly just placeholder font for testing font loading.
                     */
                     alias: "builtin_roboto_light",
-                    src: "src/static/game_assets/Roboto-Light.ttf",
+                    src: "src/static/game_assets/fonts/Roboto-Light.ttf",
                     data: { family: 'Roboto Light' }
 
                 }
@@ -72,11 +72,11 @@ export const ASSETS = {
             assets: [
                 {
                     alias: "carrot",
-                    src: "src/static/game_assets/carrot.png"
+                    src: "src/static/game_assets/collectibles/carrot.png"
                 },
                 {
                     alias: "wrench",
-                    src: "src/static/game_assets/wrench.png"
+                    src: "src/static/game_assets/collectibles/wrench.png"
                 }
             ]
         },
@@ -85,11 +85,11 @@ export const ASSETS = {
             assets: [
                 {
                     alias: "rock",
-                    src: "src/static/game_assets/Kivi3.png"
+                    src: "src/static/game_assets/obstacles/rock.png"
                 },
                 {
                     alias: "well",
-                    src: "src/static/game_assets/well.png"
+                    src: "src/static/game_assets/obstacles/well.png"
                 }
             ]
         },
@@ -97,8 +97,8 @@ export const ASSETS = {
             name: "ui",
             assets: [
                 {
-                    alias: "speechbubble_9slice",
-                    src: "src/static/game_assets/speechbubblenineslice.png"
+                    alias: "speech_bubble",
+                    src: "src/static/game_assets/ui/speech_bubble.png"
                 }
             ]
         },
