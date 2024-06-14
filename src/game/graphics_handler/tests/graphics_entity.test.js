c127b3dfa8aabd970ce93d86b8ca690599512cb2
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-06-14 11:09:42 +0300

    Added more dummy objects for gfx ent test

--- src/game/graphics_handler/tests/graphics_entity.test.js
+++ src/game/graphics_handler/tests/graphics_entity.test.js
@@ -2,6 +2,9 @@ import { GraphicsEntity } from "../entities/graphics_entity.js";
 import { EntitySkin } from "../entity_skins/entity_skin.js";
 
 describe("Test GraphicsEntity", () => {
+    let dummyTexture1;
+    let dummyTexture2;
+    let dummyTexture3;
     let skin1;
     let skin2;
     let skin3;
@@ -9,11 +12,14 @@ describe("Test GraphicsEntity", () => {
     let gfxEntity;
 
     beforeEach(() => {
-        skin1 = new EntitySkin("skin1_name", "theme1", { defaultTexture: null });
-        skin2 = new EntitySkin("skin2_name", "theme2", { defaultTexture: null });
-        skin3 = new EntitySkin("skin3_name", "theme3", { defaultTexture: null });
+        dummyTexture1 = { height: 64, widht: 64 }
+        dummyTexture2 = { height: 64, widht: 64 }
+        dummyTexture3 = { height: 64, widht: 64 }
+        skin1 = new EntitySkin("skin1_name", "theme1", { defaultTexture: dummyTexture1 });
+        skin2 = new EntitySkin("skin2_name", "theme2", { defaultTexture: dummyTexture2 });
+        skin3 = new EntitySkin("skin3_name", "theme3", { defaultTexture: dummyTexture3 });
         skinBundle = new Map([["skin1_name", skin1], ["skin2_name", skin2], ["skin3_name", skin3]]);
-        gfxEntity = new GraphicsEntity("UUID", null, { addChild: ()=>{} }, { texture: null }, null, skinBundle);
+        gfxEntity = new GraphicsEntity("UUID", null, { addChild: ()=>{}, position: null }, { texture: null }, null, skinBundle);
     });
 
     test("If when initialized, first theme is current skin", () => {
