ae071eb15be8a3ec5b8bba73bc18034f62744404
TommiTH <tommithyyrylainen@gmail.com>
2024-06-20 14:37:18 +0300

    Revert "added functions to make the file more testable"
    This reverts commit 329e9cceceab6ed4e18e6068c1086fd148c17fa9.

--- src/game/gridfactory.js
+++ src/game/gridfactory.js
@@ -10,15 +10,18 @@ import { GameModeGetCollectibles, GameModeMultipleChoice } from "./gameModes.js"
  */
 export function getGameTask() {
     //taskFactory or gridfactory?
+    let player = getNewGridObject(Constants.PLAYER_STR);
     const task = globals.task;
     const playerStartPosition = task.getPlayerStartPosition();
     const gridWidth = task.getGridDimensions().width;
     const gridHeight = task.getGridDimensions().height;
 
     let gridIntTable = globals.task.getGrid();
-    let newGrid = getNewGrid(gridWidth, gridHeight, playerStartPosition.y, playerStartPosition.x);
+    let newGrid = new Grid(player, gridWidth, gridHeight);
 
+    newGrid.addToGrid(player, playerStartPosition.y, playerStartPosition.x);
     buildGrid(gridIntTable, newGrid, gridWidth, gridHeight);
+    newGrid.saveCurrentStateForReset(); //Important!
     //make new gamemode last, make it here cause the info on gamemode is contained in the json?
     let currentGameMode;
     if (task.getMultipleChoiceQuestions().length > 0) {
@@ -31,29 +34,23 @@ export function getGameTask() {
     return { grid: newGrid, gameMode: currentGameMode };
 };
 
-function getNewGrid(gridWidth, gridHeight, playerStartPositionY, playerStartPositionX) {
-    let player = getNewGridObject(Constants.PLAYER_STR);
-    let newGrid = new Grid(player, gridWidth, gridHeight);
-    newGrid.addToGrid(player, playerStartPositionY, playerStartPositionX);
-    return newGrid;
-}
-
 /**
  * Builds the grid. Doesn't add Player.
  * @param {array} gridIntTable | double array of grid objects represented by numbers
- * @param {Grid} grid grid to add the new gridobjects. Expects the dimensions to match the gridIntTable.
+ * @param {Grid} newGrid 
+ * @param {number} width
+ * @param {number} height 
  */
-function buildGrid(gridIntTable, grid) {
-    for (let y = 0; y < grid.height; y++) {
-        for (let x = 0; x < grid.width; x++) {
+function buildGrid(gridIntTable, newGrid, width, height) {
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
             if (gridIntTable[y][x] === 2) {
                 let collectible = getNewGridObject(Constants.COLLECTIBLE);
-                grid.addToGrid(collectible, x, y);
+                newGrid.addToGrid(collectible, x, y);
             } else if (gridIntTable[y][x] === 3) {
                 let obstacle = getNewGridObject(Constants.OBSTACLE);
-                grid.addToGrid(obstacle, x, y);
+                newGrid.addToGrid(obstacle, x, y);
             }
         }
     }
-    grid.saveCurrentStateForReset(); //Important!
 }
