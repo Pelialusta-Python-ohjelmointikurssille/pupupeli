c0b822b074571c074ae3ec1c241204c643dc3354
Joonas Honkavaara <joonas.honkavaara@hotmail.com>
2024-06-24 15:53:26 +0300

    Moved functions to different file during refactoring and added exports to functions

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -7,19 +7,10 @@ import { initWorker } from "../event_handler.js";
 import { extractErrorDetails } from "../input/py_error_handling.js"
 import { disablePlayButton, initializeEditorButtons } from "./ui_editor_buttons.js";
 import { initGame, setTheme } from "../game/game_controller.js";
+import { createTaskButtons, createChapterButtons, createInstructionPage } from "./ui_buttons.js";
 
-
-const chapterDir = "/tasks/" + globals.chapterIdentifier;
-const countTaskResponse = fileReader.countForTaskFilesInDirectory(chapterDir);
-const totalTasks = countTaskResponse.count;
-const instructionTasks = countTaskResponse.instructionNumbers;
-
-const totalChapters = fileReader.countForChaptersInDirectory();
-
-let currentChapter = globals.chapterIdentifier;
 const instructionsStr = "instructions";
-
-
+let currentChapter = globals.chapterIdentifier;
 /**
  * Runs ui initialisation functions + atm the event_handlers worker
  */
@@ -108,58 +99,10 @@ function createGamePage() {
 }
 
 /**
- * Clears app-container, creates a new one and adds elements for instruction page
- */
-function createInstructionPage() {
-    const taskIdentifier = globals.taskIdentifier;
-    const chapterIdentifier = globals.chapterIdentifier;
-    const appDiv = document.getElementById("app-container");
-    appDiv.classList.add("is-hidden");
-
-    let insAppDiv = document.createElement('div');
-    insAppDiv.id = 'instructions-container';
-    insAppDiv.classList.add("box");
-    insAppDiv.style.flexDirection = "row";
-    insAppDiv.style.display = "flex";
-    window.addEventListener('load', function () {
-        createTaskButtons("instructions"); // must be called here to avoid race condition where token (retrieved from api after login) doesn't exist before the function is called
-    });
-    const insDiv = document.createElement('div');
-    insDiv.id = 'instruction-div';
-    
-    let insHead = document.createElement('div');
-    insHead.id = 'instruction-head';
-
-    let insHeadline = document.createElement('h1');
-    
-    let instructionTitle = document.createElement('a');
-    instructionTitle.id = 'instructionTitle';
-    setTitle(instructionTitle);
-    
-    insHeadline.appendChild(instructionTitle);
-
-    insHead.appendChild(insHeadline);
-    
-    let insDesc = document.createElement('div');
-    setDescription(insDesc);
-    insDesc.id = 'instruction-desc';
-    
-    
-    appDiv.insertAdjacentElement("afterend", insAppDiv);
-    insAppDiv.appendChild(insDiv);
-    insDiv.appendChild(insHead);
-    insDiv.appendChild(insDesc);
-
-    // set previous/next task button eventlisteners
-    Array.from(document.getElementsByClassName("task-navigation-button")).forEach(button => {
-        button.addEventListener("click", moveToTask);
-    });
-}
-/**
  * Sets the text from task.title to given div. Useful since game and instructions tasks use different title div.
  * @param {object} titleDiv | the title div where we want title text as a html element
  */
-function setTitle(titleDiv) {
+export function setTitle(titleDiv) {
     let chapterAndTaskNumberPeriod = `${globals.chapterIdentifier}.${globals.taskIdentifier}. `
     let titleStr = chapterAndTaskNumberPeriod + globals.task.getTitle();
     titleDiv.innerHTML = titleStr;
@@ -210,126 +153,6 @@ function colorSelectedChoice(selectedChoice) {
 
     selectedChoice.target.classList.add("selected-choice");
 }
-
-/**
- * Create buttons for selecting tasks based on how many json files exist in tasks directory.
- * In the future the path should be able to check different directories so we can implement "chapters".
- */
-function createTaskButtons(str="") {
-    const buttonContainer = document.getElementById('buttonTable');
-    buttonContainer.innerHTML = ''
-
-    if (localStorage.getItem("token") !== null) { // user is logged in
-        api.getCompletedTasks().then(taskList => {
-            let completedTasksList = taskList.tasks;
-            // Create and append buttons
-            for (let i = 0; i < totalTasks; i++) {
-                const button = document.createElement('button');
-                let taskNumber = i+1;
-            // if task is task, increase tasknumber.
-            // if task is instruction, increase instructionnumber and dont increase tasknumber.
-            if (instructionTasks.includes(taskNumber)) {
-                button.innerText = `${taskNumber}i`;
-            } else {
-                button.innerText = `${taskNumber}`;
-            }
-
-            let buttonIdText = `chapter${currentChapter}task${i + 1}`;
-                button.id = buttonIdText
-                if (currentChapter === globals.chapterIdentifier && i+1 === globals.taskIdentifier) {
-                    button.classList.add("button-current-task")
-                }
-                if (completedTasksList.includes(buttonIdText)) {
-                    button.classList.add("button-completed");
-                } else {
-                    button.classList.add("button-incompleted");
-                }
-                    button.addEventListener('click', () => {
-                    window.location.href = `?chapter=${currentChapter}&task=${i + 1}`;
-                });
-                buttonContainer.appendChild(button);
-            }
-            if (str === "instructions") {
-            onTaskComplete(true);
-        }
-    });
-    } else { // user is not logged in
-        for (let i = 0; i < totalTasks; i++) {
-            const button = document.createElement('button');
-            let taskNumber = i+1;
-            let buttonIdText = `chapter${currentChapter}task${i + 1}`;
-            button.id = buttonIdText
-            button.classList.add("button-incompleted");
-            if (instructionTasks.includes(taskNumber)) {
-                button.innerText = `${taskNumber}i`;
-            } else {
-                button.innerText = `${taskNumber}`;
-            }
-            button.addEventListener('click', () => {
-                window.location.href = `?chapter=${currentChapter}&task=${i + 1}`;
-            });
-            buttonContainer.appendChild(button);
-        }
-    }
-}
-
-function createChapterButtons() {
-    const selectContainer = document.getElementById('chapterbuttontable');
-
-    selectContainer.innerHTML = '';
-
-    if (localStorage.getItem("token") !== null) {
-        api.getCompletedTasks().then(taskList => {
-            let completedTasksList = taskList.tasks;
-            for (let i = 0; i < totalChapters; i++) {
-                const button = document.createElement('button');
-                button.id = `chapter-button-${i + 1}`;
-                button.value = i + 1;
-                button.innerText = `Tehtäväsarja ${i + 1}`;
-                if (currentChapter === i + 1) button.classList.add("button-current-chapter")
-                //Check if 
-                let currentTotalTasks = fileReader.countForTaskFilesInDirectory("/tasks/" + (i + 1)).count;
-                let allTasksCompleted = true;
-                for (let j = 0; j < currentTotalTasks; j++) {
-                    let taskId = `chapter${i + 1}task${j + 1}`;
-                    if (!completedTasksList.includes(taskId)) {
-                        allTasksCompleted = false;
-                        break;
-                    }
-                }
-
-                if (allTasksCompleted) {
-                    button.classList.add("button-completed");
-                } else {
-                    button.classList.add("button-incompleted");
-                }
-
-                button.addEventListener('click', () => {
-                    currentChapter = i + 1;
-                    console.log(currentChapter)
-                    window.location.href = `/?chapter=${currentChapter}&task=1`;
-                });
-                selectContainer.appendChild(button);
-            }
-        })
-    } else {
-        for (let i = 0; i < totalChapters; i++) {
-            const button = document.createElement('button');
-            button.id = `chapter-button-${i + 1}`;
-            button.value = i + 1;
-            button.innerText = `Tehtäväsarja ${i + 1}`;
-            if (currentChapter === i + 1) button.classList.add("button-current-chapter")
-
-            button.addEventListener('click', () => {
-                currentChapter = i + 1;
-                console.log(currentChapter)
-                window.location.href = `/?chapter=${currentChapter}&task=1`;
-            });
-            selectContainer.appendChild(button);
-        }
-    } 
-}
-
 
 /**
  * a function that turns the current button green and sends a PUSH request to the api indicating the user has completed a task
@@ -387,7 +210,7 @@ export function onTaskComplete(won) {
 /**
  * a function used by the previous/next task buttons on the page
  */
-function moveToTask(event) {
+export function moveToTask(event) {
     let currentTask = globals.taskIdentifier;
     let which = event.target.value;
     if (currentTask === 1 && which === "previous") return;
