1a7c36e3e4fd6a45f3861c3de556cb9e29126e14
TommiTH <tommithyyrylainen@gmail.com>
2024-06-27 14:27:13 +0300

    updated docstrings in cell.js

--- src/game/cell.js
+++ src/game/cell.js
@@ -1,7 +1,6 @@
-// src/game/cell.js
-// Write docs for cell class
 /**
-* Represents a cell in the game grid 
+* Represents a cell in the game grid. It's a list containing all the gridobjects in that position in a grid.
+* This can be used for having multiple objects in the same grid, (special floors for player for example).
 * @param {number} x - The x coordinate of the cell
 * @param {number} y - The y coordinate of the cell
 * @class
@@ -13,7 +12,6 @@
 * @method getEntities - Returns the entities in the cell
 * @returns {Cell} - A cell object
 */
-
 export class Cell {
     constructor(x, y) {
         this.x = x;
