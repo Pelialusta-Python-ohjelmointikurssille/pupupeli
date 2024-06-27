14637f5fc847e0c5e33964c09fa4a2c306edbd69
TommiTH <tommithyyrylainen@gmail.com>
2024-06-27 14:09:48 +0300

    added docstrings to game_controller.js

--- src/game/game_controller.js
+++ src/game/game_controller.js
@@ -39,22 +39,41 @@ export function commandsDone() {
     postMessage({ type: "return", details: "returning from game.js", sab: currentCommand.sab });
 }
 
+/**
+* @returns Gets the pixiApp.canvas.
+*/
 export function getCanvas() {
     game.getCanvas();
 }
 
+//Problem: below there are 4 functions that do nothing but call the games functions of the same name.
+//Maybe do what they do here, it feels stupid to copypaste docstring.
+
+/**
+* Restores the gamestate back to the beginning of the task.
+*/
 export function resetGame() {
     game.resetGame();
 }
 
+/**
+* Changes theme, currently uses the finnish name of the theme
+* @param {*} theme theme to change into in finnish
+*/
 export function setTheme(theme) {
     game.setTheme(theme);
 }
 
+/**
+ * Toggles a grid that is drawn to see the grid easier and the indexes of the x and y in the grid
+ */
 export function toggleGrid() {
     game.toggleGrid();
 }
 
+/**
+ * Toggles the rendering of the trail on and off. It draws a line representing the route of player in the grid. 
+ */
 export function toggleTrail() {
     game.toggleTrail();
 }
@@ -72,6 +91,11 @@ export function getGridObjectsOfTypeLeft(type) {
     return game.grid.data.getGridObjectsOfTypeCount(type);
 }
 
+
+/**
+ * Makes game create an object, chops the commandParameters to an understandable form for game.
+ * @param {*} commandParameters 
+ */
 export function createObject(commandParameters) {
     let name = commandParameters[0]
     let x = commandParameters[1];
@@ -79,7 +103,10 @@ export function createObject(commandParameters) {
     name = getVariableTrueName(name);
     game.createNewPlayerCreatedGridObject(name, x, y);
 }
-
+/**
+ * Makes game destroy an object, chops the commandParameters to an understandable form for game.
+ * @param {*} commandParameters 
+ */
 export function destroyObject(commandParameters) {
     let x = commandParameters[0];
     let y = commandParameters[1];
