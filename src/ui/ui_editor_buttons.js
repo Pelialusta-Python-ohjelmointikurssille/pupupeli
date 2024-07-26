3d0eca10d97c4f1257d461d0217de7857dcfbd1a
VP-MaxHax <vp.juselius@gmail.com>
2024-07-26 11:11:02 +0300

    removed now useless alert popup for lost editor code on theme switch

--- src/ui/ui_editor_buttons.js
+++ src/ui/ui_editor_buttons.js
@@ -192,11 +192,6 @@ function initThemeSelect() {
     themeSelectDropdown = document.getElementById("theme-select");
     themeSelectDropdown.value = localStorage.getItem("theme");
     themeSelectDropdown.addEventListener('change', function (event) {
-        let isUserSure = window.confirm("Oletko varma että haluat vaihtaa teeman? Teeman vaihtaminen nollaa tehtävään kirjoitetun koodin.");
-        if (isUserSure === false) {
-            themeSelectDropdown.value = localStorage.getItem("theme");
-            return;
-        }
         let selectedValue = event.target.value;
         setCurrentTheme(selectedValue);
         themeChangeToWorker()
