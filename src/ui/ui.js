162bbd2ba392cedc78bb2905d775a685c478ae7b
TommiTH <tommithyyrylainen@gmail.com>
2024-09-07 16:51:28 +0300

    removed unused variable

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -120,7 +120,6 @@ export function setTitle(titleDiv) {
 
 export async function setEditorCode() {
     let editorCode = "";
-    let isEditorCodeLoaded = false;
     showCodeBlocksInsteadOfEditor(false);
     console.log(globals.task);
     if (localStorage.getItem("token")) {
@@ -137,7 +136,6 @@ export async function setEditorCode() {
                 showCodeBlocksInsteadOfEditor(true);
             }
         });
-        isEditorCodeLoaded = true;
     } else {
         editorCode = globals.task.getEditorCode();
         getEditor().setValue(editorCode);
